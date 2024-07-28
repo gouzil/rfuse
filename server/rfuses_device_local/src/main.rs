1740c72e524c23922af3bb78256cb5facf91e576
gouzil <66515297+gouzil@users.noreply.github.com>
2024-07-29 00:30:43 +0800

    ✨ feat(device_local):add init log (#2)

--- server/rfuses_device_local/src/main.rs
+++ server/rfuses_device_local/src/main.rs
@@ -3,9 +3,32 @@ use rfuse_core::sys_fs::RFuseFS;
 use rfuses_device_local::init_fs::user_defined_init_fs;
 use rfuses_device_local::local_fs::LocalFS;
 
+// 日志初始化
+#[cfg(debug_assertions)]
+fn init_log() {
+    env_logger::init();
+}
+
+#[cfg(not(debug_assertions))]
+fn init_log() {
+    use flexi_logger::{Cleanup, Criterion, FileSpec, Logger, Naming, WriteMode};
+    Logger::try_with_str("info")
+        .unwrap()
+        .log_to_file(FileSpec::default().directory("./logs").suppress_timestamp()) // 日志文件目录
+        .write_mode(WriteMode::BufferAndFlush) // 有缓存的写入模式
+        // .write_mode(WriteMode::Direct) // 直接写入模式
+        .rotate(
+            Criterion::Size(10 * 1024 * 1024), // 每个文件 10M
+            Naming::Numbers,
+            Cleanup::KeepLogFiles(7),
+        )
+        .start()
+        .unwrap();
+}
+
 // 使用 walkdir 和 nix 实现 local 的文件系统, 尽量避开特定操作系统
 fn main() {
-    env_logger::init();
+    init_log();
     let options = vec![
         MountOption::RW, // 这样才是读写
         // MountOption::RO, // 这样是只读
