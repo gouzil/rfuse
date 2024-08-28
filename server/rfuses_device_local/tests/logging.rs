b2de00f834ca7a94acea9f6060309288ef435107
gouzi <530971494@qq.com>
2024-08-28 23:56:30 +0800

    fix

--- server/rfuses_device_local/tests/logging.rs
+++ server/rfuses_device_local/tests/logging.rs
@@ -1,11 +1,25 @@
+use chrono::Utc;
 use common::{rfuses_spawn_run, run_command_with_status, TestContext};
+use directories::ProjectDirs;
 
 mod common;
 
 #[tokio::test]
 async fn test_logging_verbose() {
     let context = TestContext::new();
-    let closure = || {};
+    let closure = || {
+        let base_path = ProjectDirs::from("", "", "rfuse").unwrap();
+        let mut log_dir_path = base_path.runtime_dir().unwrap().to_owned();
+        log_dir_path.push("logs/");
+        let log_file = log_dir_path.join(format!("{}-rfuses.log", Utc::now().format("%Y-%m-%d")));
+        assert!(log_dir_path.is_dir());
+        assert!(log_file.is_file());
+
+        let log_content = std::fs::read_to_string(log_file).unwrap();
+        assert!(log_content.contains("INFO"));
+        assert!(log_content.contains("DEBUG"));
+        assert!(log_content.contains("File system init success"));
+    };
     rfuses_spawn_run!(
         {
             context
