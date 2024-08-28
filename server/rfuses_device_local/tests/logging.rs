7c605b05caa9d4651140fac7ac605f24c237dacc
gouzi <530971494@qq.com>
2024-08-29 00:05:39 +0800

    fix

--- server/rfuses_device_local/tests/logging.rs
+++ server/rfuses_device_local/tests/logging.rs
@@ -8,17 +8,20 @@ mod common;
 async fn test_logging_verbose() {
     let context = TestContext::new();
     let closure = || {
-        let base_path = ProjectDirs::from("", "", "rfuse").unwrap();
-        let mut log_dir_path = base_path.runtime_dir().unwrap().to_owned();
-        log_dir_path.push("logs/");
-        let log_file = log_dir_path.join(format!("{}-rfuses.log", Utc::now().format("%Y-%m-%d")));
-        assert!(log_dir_path.is_dir());
-        assert!(log_file.is_file());
+        if cfg!(debug_assertions) {
+            let base_path = ProjectDirs::from("", "", "rfuse").unwrap();
+            let mut log_dir_path = base_path.runtime_dir().unwrap().to_owned();
+            log_dir_path.push("logs/");
+            let log_file =
+                log_dir_path.join(format!("{}-rfuses.log", Utc::now().format("%Y-%m-%d")));
+            assert!(log_dir_path.is_dir());
+            assert!(log_file.is_file());
 
-        let log_content = std::fs::read_to_string(log_file).unwrap();
-        assert!(log_content.contains("INFO"));
-        assert!(log_content.contains("DEBUG"));
-        assert!(log_content.contains("File system init success"));
+            let log_content = std::fs::read_to_string(log_file).unwrap();
+            assert!(log_content.contains("INFO"));
+            assert!(log_content.contains("DEBUG"));
+            assert!(log_content.contains("File system init success"));
+        }
     };
     rfuses_spawn_run!(
         {
